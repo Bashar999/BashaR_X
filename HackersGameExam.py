2ab009da7fd7a05f6b6fce7a4a624a2260a57af7
Bashar999 <49046030+Bashar999@users.noreply.github.com>
2020-06-09 13:34:20 +0200

    Add files via upload

---
+++ HackersGameExam.py
@@ -0,0 +1,158 @@
+print ("created BY : \n")
+
+print (" ____            _           ____    __  __ ")
+print ("| __ )  __   ___| |__   __ _|  _ \   \ \/ / ")
+print ("|  _ \ / _` / __| '_ \ / _` | |_) |   \  /  ")
+print ("| |_) | (_| \__ \ | | | (_| |  _ <    /  \ ")
+print ("|____/ \__,_|___/_| |_|\__,_|_| \_\  /_/\_\ \n")
+
+print ("\n")
+ready = str(input("ARE YOU READY ? (yes or no)  \n"))
+if ready == "yes" :
+    print (" _     _____ _____ ____    ____ _____  _    ____ _____ ")
+    print ("| |   | ____|_   _/ ___|  / ___|_   _|/ \  |  _ \_   _|")
+    print ("| |   |  _|   | | \___ \  \___ \ | | / _ \ | |_) || |  ")
+    print ("| |___| |___  | |  ___) |  ___) || |/ ___ \|  _ < | |  ")
+    print ("|_____|_____| |_| |____/  |____/ |_/_/   \_\_| \_\|_|  ")
+print ("\n")
+if ready == "no"  :
+    print ("NO , YOU ARE READY , LETS GO ! \n")
+    print ("\n")
+print ("qustion 1 \n")
+qustion1 = str(input("whats the comand you write to open Metasploit  ?  \n"))
+if qustion1 == "msfconsole" :
+    print ("\n")
+    print ("Right answer \n")
+    print ("qustion 2")
+
+qustion2 = "turn off the internet"
+qustion2 = str(input("what is the first thing you should do if someone hacked your device ?  \n"))
+if qustion2 == "turn off the internet" :
+    print ("\n")
+    print ("Right answer \n")
+    print ("qustion 3")
+
+qustion3 = "beef"
+qustion3 = str(input("whats the command you write to open Beef-FrameWork  ? \n"))
+if qustion3 == "beef" :
+    print ("\n")
+    print ("Right answer")
+    print ("qustion 4 \n")
+
+print ("which one of this tools used to crack on socialmedia accounts ? \n")
+print ("Crunch \n")
+print ("SocialBox \n")
+print ("SQLmap \n")
+print ("HiddenEye \n")
+qustion4 = str(input(" choose :-->>  "))
+if qustion4 == "SocialBox" :
+    print ("\n")
+    print ("Right answer ! \n")
+    print ("qustion 5")
+
+print (" Which one of this tools create a fake pages for all kinds of sites ? \n")
+print ("Ngrok \n")
+print ("Metasploit \n")
+print ("HiddenEye \n")
+print ("Crunch \n")
+qustion5 = str(input(" choose :-->>  "))
+if qustion5 == "HiddenEye" :
+    print ("\n")
+    print ("Right answer ! \n")
+    print ("qustion 6")
+
+print ("what's the most programming language has used in cyber security and penetration test ? \n")
+print ("HTML \n")
+print ("C# \n")
+print ("Java \n")
+print ("Python \n")
+qustion6 = str(input(" choose :-->>  "))
+if qustion6 == "Python" :
+    print ("\n")
+    print ("Right answer \n")
+    print ("qustion 7 \n")
+
+print ("Select the two languages that are include in Metasploit programming : \n")
+print ("java and C \n")
+print ("Python and Ruby \n")
+print ("HTML and CSS \n")
+print ("C and python \n")
+qustion7 = str(input(" choose :-->>  "))
+if qustion7 == "Python and Ruby" :
+    print ("\n")
+    print ("Right answer ! \n")
+    print ("qustion 8")
+
+print ("which one of this tools used to crack on wifi password ? \n")
+print ("wifi phisher \n")
+print ("Aircrack-ng \n")
+print ("Metasploit FrameWork \n")
+print ("ip-jetable \n")
+qustion8 = str(input(" choose :-->>  "))
+if qustion8 == "Aircrack-ng" :
+    print ("\n")
+    print ("Right Answer ! \n")
+    print ("qustion 9")
+
+print ("which one of this FrameWorks has more Exploits ? \n")
+print ("Xerosploit \n")
+print ("Findsploit \n")
+print ("Metasploit \n")
+print ("Kali linux \n")
+qustion9 = str(input(" choose :-->>  "))
+if qustion9 == "Metasploit" :
+    print ("\n")
+    print ("Right Answer ! \n")
+    print ("qustion 10")
+
+print ("What's the difference between ip and ip address ? \n")
+qustion10 = str(input(" choose :-->>  "))
+if qustion10 == "nothing" :
+    print ("\n")
+    print ("Right Answer \n")
+    print ("YOU ARE GENIUS")
+    print ("You Should Work with Anonymous ;) \n")
+
+print (" _   _  _____        __  ___ _____  __        _____ _     _       ____  _____ ")
+print ("| \ | |/ _ \ \      / / |_ _|_   _| \ \      / /_ _| |   | |     | __ )| ____|")
+print ("|  \| | | | \ \ /\ / /   | |  | |    \ \ /\ / / | || |   | |     |  _ \|  _|  ")
+print ("| |\  | |_| |\ V  V /    | |  | |     \ V  V /  | || |___| |___  | |_) | |___ ")
+print ("|_| \_|\___/  \_/\_/    |___| |_|      \_/\_/  |___|_____|_____| |____/|_____|")
+print (" _   _    _    ____  ____      ___  _   _  ___ _____ ____ __  _   _ ____")
+print ("| | | |  / \  |  _ \|  _ \    / _ \| | | / ___|_   _|_ _/ _ \| \ | / ___| ")
+print ("| |_| | / _ \ | |_) | | | |  | | | | | | \___ \ | |  | | | | |  \| \___ \ ")
+print ("|  _  |/ ___ \|  _ <| |_| |  | |_| | |_| |___) || |  | | |_| | |\  |___) |")
+print ("|_| |_/_/   \_\_| \_\____/    \__\_\\___/|____/ |_| |___\___/|_| \_|____/ ")
+
+print ("\n")
+
+mession = str(input("Are Ready For That ? (yes or no) \n"))
+if mession == "yes" :
+    print ("\n")
+    print (" __  __ _____ ____ ____ ___ ___  _   _   ____  ")
+    print ("|  \/  | ____/ ___/ ___|_ _/ _ \| \ | | |___ \ ")
+    print ("| |\/| |  _| \___ \___ \| | | | |  \| |   __) |")
+    print ("| |  | | |___ ___) |__) | | |_| | |\  |  / __/ ")
+    print ("|_|  |_|_____|____/____/___\___/|_| \_| |_____|")
+
+print ("\n")
+
+print (" ____ _____  _    ____ _____ ")
+print ("/ ___|_   _|/ \  |  _ \_   _|")
+print ("\___ \ | | / _ \ | |_) || |  ")
+print (" ___) || |/ ___ \|  _ < | | ") 
+print ("|____/ |_/_/   \_\_| \_\|_|  ")
+
+print ("\n")
+print ("\n")
+
+qustion10 = str(input("What do you want to eat now ? "))
+print ("\n")
+print ("good , me to")
+
+
+
+
+
+
+
